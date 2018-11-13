bef56d8ce6d703953f70167b5ecd1cc0a0ff709e
yuanwh15 <43902486+yuanwh15@users.noreply.github.com>
2018-11-13 14:34:24 +0800

    Add files via upload
    实现search功能

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,26 +1,51 @@
-// pages/index/index.js
-var app = getApp()
+// pages/search/search.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    bindKeyInput: null,
+    result: []
+  },
+  bindKeyInput: function (e) {
+    console.log(e);
+    this.search(e.detail.value)
+    this.data.bindKeyInput = e.detail.value;
+    console.log(this.data.bindKeyInput);
+  },
+  search: function (key) {
+    var site = app.globalData.site;
+    var arr = [];
+    for (let i in site) {
+      if (site[i].name.indexOf(key) >= 0) {
+        arr.push(site[i])
+      }
+    }
+    this.setData({
+      result: arr
+    })
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var result = [];
+    this.setData({
+      result:result
+    })
+
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
@@ -64,9 +89,15 @@ Page({
   onShareAppMessage: function () {
 
   },
-  goToMap: function (param) {
+  jumpDetails: function (e) {
+    console.log(e.currentTarget.id);
     wx.navigateTo({
-      url: '/pages/map/map',
+      url: '../details/details?index=' + e.currentTarget.id,
+    })
+  },
+  jumpMap: function (e) {
+    wx.navigateTo({
+      url: '../map/map?id=' + e.currentTarget.id,
     })
   }
 })
